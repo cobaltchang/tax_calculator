944e30fb40eededc2d44c3d8ba4601cfa46b59ed
Cobalt Chang <cobaltchang@nexcom.com.tw>
2018-04-27 16:03:26 +0800

    1. Revise description of test specs.
    2. Add gray-box test specs.
    
    	modified:   tests/test_tax_calculator.py

--- tests/test_tax_calculator.py
+++ tests/test_tax_calculator.py
@@ -20,6 +20,54 @@ class 計算單人稅額(unittest.TestCase):
 
         self.總免稅額 = 免稅額 + 標準扣除額 + 特別扣除額
 
+    """
+    灰箱測試
+    """
+    def test_所得總額為50萬課稅級距為百分之五(self):
+        income = 500000
+
+        tax = calc_tax(income)
+
+        self.assertEqual(tax, 9700)
+
+    def test_所得總額為100萬則超過先前級距的部份課稅級距為百分之十二(self):
+        income = 1000000
+
+        tax = calc_tax(income)
+
+        self.assertEqual(tax, 45480)
+
+    def test_所得總額為200萬則超過先前級距的部份課稅級距為百分之二十(self):
+        income = 2000000
+
+        tax = calc_tax(income)
+
+        self.assertEqual(tax, 204200)
+
+    def test_所得總額為300萬則超過先前級距的部份課稅級距為百分之三十(self):
+        income = 3000000
+
+        tax = calc_tax(income)
+
+        self.assertEqual(tax, 431600)
+
+    def test_所得總額為500萬則超過先前級距的部份課稅級距為百分之四十(self):
+        income = 5000000
+
+        tax = calc_tax(income)
+
+        self.assertEqual(tax, 1048000)
+
+    def test_所得總額為3000萬則超過先前級距的部份課稅級距為百分之四十五(self):
+        income = 30000000
+
+        tax = calc_tax(income)
+
+        self.assertEqual(tax, 12017200)
+
+    """
+    白箱測試
+    """
     def test_所得總額不高於免稅額加各項扣除額時為免稅(self):
         income = self.總免稅額
 
@@ -41,63 +89,63 @@ class 計算單人稅額(unittest.TestCase):
 
         self.assertEqual(tax, 27000)
 
-    def test_所得淨額為_540001_則超過_540000_的部份課稅級距為百分之十二(self):
+    def test_所得淨額為_540001_則超過先前級距的部份課稅級距為百分之十二(self):
         income = self.總免稅額 + 540001
 
         tax = calc_tax(income)
 
         self.assertEqual(tax, 27000)
 
-    def test_所得淨額為_1210000_則超過_540000_的部份課稅級距為百分之十二(self):
+    def test_所得淨額為_1210000_則超過先前級距的部份課稅級距為百分之十二(self):
         income = self.總免稅額 + 1210000
 
         tax = calc_tax(income)
 
         self.assertEqual(tax, 107400)
 
-    def test_所得淨額為_1210001_則超過_1210000_的部份課稅級距為百分之二十(self):
+    def test_所得淨額為_1210001_則超過先前級距的部份課稅級距為百分之二十(self):
         income = self.總免稅額 + 1210001
 
         tax = calc_tax(income)
 
         self.assertEqual(tax, 107400)
 
-    def test_所得淨額為_2420000_則超過_1210000_的部份課稅級距為百分之二十(self):
+    def test_所得淨額為_2420000_則超過先前級距的部份課稅級距為百分之二十(self):
         income = self.總免稅額 + 2420000
 
         tax = calc_tax(income)
 
         self.assertEqual(tax, 349400)
 
-    def test_所得淨額為_2420001_則超過_2420000_的部份課稅級距為百分之三十(self):
+    def test_所得淨額為_2420001_則超過先前級距的部份課稅級距為百分之三十(self):
         income = self.總免稅額 + 2420001
 
         tax = calc_tax(income)
 
         self.assertEqual(tax, 349400)
 
-    def test_所得淨額為_4530000_則超過_2420000_的部份課稅級距為百分之三十(self):
+    def test_所得淨額為_4530000_則超過先前級距的部份課稅級距為百分之三十(self):
         income = self.總免稅額 + 4530000
 
         tax = calc_tax(income)
 
         self.assertEqual(tax, 982400)
 
-    def test_所得淨額為_4530001_則超過_4530000_的部份課稅級距為百分之四十(self):
+    def test_所得淨額為_4530001_則超過先前級距的部份課稅級距為百分之四十(self):
         income = self.總免稅額 + 4530001
 
         tax = calc_tax(income)
 
         self.assertEqual(tax, 982400)
 
-    def test_所得淨額為_10310000_則超過_4530000_的部份課稅級距為百分之四十(self):
+    def test_所得淨額為_10310000_則超過先前級距的部份課稅級距為百分之四十(self):
         income = self.總免稅額 + 10310000
 
         tax = calc_tax(income)
 
         self.assertEqual(tax, 3294400)
 
-    def test_所得淨額為_10310001_則超過_10310000_的部份課稅級距為百分之四十五(self):
+    def test_所得淨額為_10310001_則超過先前級距的部份課稅級距為百分之四十五(self):
         income = self.總免稅額 + 10310001
 
         tax = calc_tax(income)
