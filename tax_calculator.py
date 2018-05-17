7a5a8f90e7c1da1981e5f95dee55c11a9d367faa
Cobalt Chang <cobaltchang@nexcom.com.tw>
2018-05-17 17:22:46 +0800

    1. Implemnt from 4a4g0008.
    	modified:   tax_calculator.py

--- tax_calculator.py
+++ tax_calculator.py
@@ -1,2 +1,16 @@
 def calc_tax(income):
-    return 0
+	income = income - 306000
+	if income <= 540000:
+		return int(income*0.05)
+	elif income <= 1210000:
+		return int(income*0.12)-37800
+	elif income <= 2420000:
+		return int(income*0.20)-134600
+	elif income <= 4530000:
+		return int(income*0.30)-376600
+	elif income <= 10310000:
+		return int(income*0.40)-829600
+	elif income > 10310000:
+		return int((income *0.45))-1345100
+	else:
+		return 0
