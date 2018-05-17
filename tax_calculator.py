d4422293563d3c2cec1fa4b65312d991812800bc
Cobalt Chang <cobaltchang@nexcom.com.tw>
2018-05-17 17:31:09 +0800

    1. Implement from 4a4g0013.
    	modified:   tax_calculator.py

--- tax_calculator.py
+++ tax_calculator.py
@@ -1,2 +1,20 @@
 def calc_tax(income):
-    return 0
+    free = 88000 + 90000 + 128000
+    income_2 = income - free
+
+    if income <= 540000:
+        return int(income_2*0.05)
+    elif income <= 1210000:
+        return int(income_2*0.12-37800)
+    elif income <= 2420000:
+        return int(income_2*0.2-134600)
+    elif income <= 4530000:
+        return int(income_2*0.3-376600)
+    elif income <= 10310000:
+        return int(income_2*0.4-829600)
+    elif income > 10310000:
+        return int(income_2*0.45-1345100)
+    else:
+        return 0
+
+
